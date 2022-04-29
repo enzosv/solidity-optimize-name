c5b8c7812b705ca5e1f22883756920984c1f36a9
enzosv <vergara.enzo@gmail.com>
2022-04-30 00:32:13 +0800

    offload expensive task to worker

--- app.js
+++ app.js
@@ -1,52 +1,4 @@
 (function ($) {
-  var CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  var CHAR_MAP = {};
-  CHARS.forEach(function (c, index) {
-    CHAR_MAP[c] = index;
-  });
-
-  var CHAR_CODE_MAP = {};
-  CHARS.forEach(function (c, index) {
-    CHAR_CODE_MAP[index] = c.charCodeAt(0);
-  });
-
-  var data = { blocks: [], s: [] };
-  function save(hash) {
-    data.reset = hash.reset;
-    data.block = hash.block;
-    data.start = hash.start;
-    data.finalized = hash.finalized;
-    data.lastByteIndex = hash.lastByteIndex;
-    for (var i = 0; i < hash.blocks.length; ++i) {
-      data.blocks[i] = hash.blocks[i];
-    }
-    for (var i = 0; i < hash.s.length; ++i) {
-      data.s[i] = hash.s[i];
-    }
-  }
-
-  function restore(hash) {
-    hash.reset = data.reset;
-    hash.block = data.block;
-    hash.start = data.start;
-    hash.finalized = data.finalized;
-    hash.lastByteIndex = data.lastByteIndex;
-    for (var i = 0; i < data.blocks.length; ++i) {
-      hash.blocks[i] = data.blocks[i];
-    }
-    for (var i = 0; i < data.s.length; ++i) {
-      hash.s[i] = data.s[i];
-    }
-  }
-
-  function toBytes(str) {
-    var bytes = [];
-    for (var i = 0; i < str.length; ++i) {
-      bytes.push(str.charCodeAt(i));
-    }
-    return bytes;
-  }
-
   function parseSignature(signature) {
     if (signature.charAt(signature.length - 1) != ')' || signature.indexOf(' ') !== -1) {
       return false;
@@ -61,94 +13,32 @@
       return false;
     }
   }
-
-  function increase(bytes) {
-    bytes[0] += 1;
-    for (var i = 0; i < bytes.length; ++i) {
-      if (bytes[i] === 64) {
-        bytes[i] = 0;
-        if (i == bytes.length - 1) {
-          bytes[i + 1] = 1;
-        } else {
-          bytes[i + 1] += 1;
-        }
-      } else {
-        break;
-      }
-    }
-    return bytes;
-  }
-
-  function toChars(bytes) {
-    var str = '';
-    for (var i = 0; i < bytes.length; ++i) {
-      str += CHARS[bytes[i]];
-    }
-    return str;
-  }
-
-  function toCharCodes(bytes) {
-    var codes = [];
-    for (var i = 0; i < bytes.length; ++i) {
-      codes.push(CHAR_CODE_MAP[bytes[i]]);
-    }
-    return codes;
-  }
-
-  function find(obj, after) {
-    if (after == "") {
-      after = "0"
-    }
-    var sig = obj.name + obj.args;
-    var args = toBytes(obj.args);
-    var bytes = [0];
-    var index = 0;
-    var prefix = toBytes(obj.name + '_');
-    var hash = keccak256.create();
-    hash.update(prefix);
-    save(hash);
-    var char, methodId = keccak256.array(sig);
-    var id = ""
-    while (true) {
-      sig = obj.name + '_' + toChars(bytes) + char + obj.args;
-      id = keccak256(sig).substr(0, 8)
-      if (id.startsWith('0000') && id > after) {
-        break
-      }
-      if (index >= CHARS.length) {
-        increase(bytes);
-        hash = keccak256.create();
-        hash.update(prefix);
-        hash.update(toCharCodes(bytes));
-        save(hash);
-        index = 0;
-      }
-      char = CHARS[index];
-      hash.update(char);
-      hash.update(args);
-      methodId = hash.array();
-      restore(hash);
-      ++index;
-    }
-
-    $('#after').val(id)
-    return [sig, id];
-  }
-
   $(document).ready(function () {
+    if (!window.Worker) {
+      $('#error').html("your browser does not support workers")
+      $('#error').show();
+      return
+    }
+    const worker = new Worker("worker.js");
     $('#optimize').click(function () {
       var input = $('#input').val().replaceAll(" ", "");
       var after = $('#after').val();
-      $('#error').hide();;
       var data = parseSignature(input);
       if (!data) {
-        $('#error').show();;
+        $('#error').show();
         return;
       }
-      console.time('a');
-      var result = find(data, after);
-      console.timeEnd('a')
-      $('#output').append('<p>' + result[0] + ': 0x' + result[1] + '</p>')
+      $('#error').hide();
+      $('#optimize').prop("disabled", true);
+      $('#loader').show();
+      worker.postMessage([data, after])
+
     });
+    worker.onmessage = function (e) {
+      $('#after').val(e.data[1])
+      $('#output').append('<p>' + e.data[0] + ': 0x' + e.data[1] + '</p>')
+      $('#loader').hide();
+      $('#optimize').prop("disabled", false);
+    }
   });
 })(jQuery);
---
+++ worker.js
@@ -0,0 +1,123 @@
+importScripts('https://cdn.rawgit.com/emn178/js-sha3/fb7e6403/build/sha3.min.js');
+
+const CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var CHAR_MAP = {};
+CHARS.forEach(function (c, index) {
+    CHAR_MAP[c] = index;
+});
+
+var CHAR_CODE_MAP = {};
+CHARS.forEach(function (c, index) {
+    CHAR_CODE_MAP[index] = c.charCodeAt(0);
+});
+
+var data = { blocks: [], s: [] };
+function save(hash) {
+    data.reset = hash.reset;
+    data.block = hash.block;
+    data.start = hash.start;
+    data.finalized = hash.finalized;
+    data.lastByteIndex = hash.lastByteIndex;
+    for (var i = 0; i < hash.blocks.length; ++i) {
+        data.blocks[i] = hash.blocks[i];
+    }
+    for (var i = 0; i < hash.s.length; ++i) {
+        data.s[i] = hash.s[i];
+    }
+}
+
+function restore(hash) {
+    hash.reset = data.reset;
+    hash.block = data.block;
+    hash.start = data.start;
+    hash.finalized = data.finalized;
+    hash.lastByteIndex = data.lastByteIndex;
+    for (var i = 0; i < data.blocks.length; ++i) {
+        hash.blocks[i] = data.blocks[i];
+    }
+    for (var i = 0; i < data.s.length; ++i) {
+        hash.s[i] = data.s[i];
+    }
+}
+
+function toBytes(str) {
+    var bytes = [];
+    for (var i = 0; i < str.length; ++i) {
+        bytes.push(str.charCodeAt(i));
+    }
+    return bytes;
+}
+
+function increase(bytes) {
+    bytes[0] += 1;
+    for (var i = 0; i < bytes.length; ++i) {
+        if (bytes[i] === 64) {
+            bytes[i] = 0;
+            if (i == bytes.length - 1) {
+                bytes[i + 1] = 1;
+            } else {
+                bytes[i + 1] += 1;
+            }
+        } else {
+            break;
+        }
+    }
+    return bytes;
+}
+
+function toChars(bytes) {
+    var str = '';
+    for (var i = 0; i < bytes.length; ++i) {
+        str += CHARS[bytes[i]];
+    }
+    return str;
+}
+
+function toCharCodes(bytes) {
+    var codes = [];
+    for (var i = 0; i < bytes.length; ++i) {
+        codes.push(CHAR_CODE_MAP[bytes[i]]);
+    }
+    return codes;
+}
+
+onmessage = function (e) {
+    console.time('a');
+    let obj = e.data[0];
+    let after = e.data[1];
+    if (after == "") {
+        after = "0"
+    }
+    var sig = obj.name + obj.args;
+    var args = toBytes(obj.args);
+    var bytes = [0];
+    var index = 0;
+    var prefix = toBytes(obj.name + '_');
+    var hash = keccak256.create();
+    hash.update(prefix);
+    save(hash);
+    var char = keccak256.array(sig);
+    var id = ""
+    while (true) {
+        sig = obj.name + '_' + toChars(bytes) + char + obj.args;
+        id = keccak256(sig).substr(0, 8)
+        if (id.startsWith('0000') && id > after) {
+            break
+        }
+        if (index >= CHARS.length) {
+            increase(bytes);
+            hash = keccak256.create();
+            hash.update(prefix);
+            hash.update(toCharCodes(bytes));
+            save(hash);
+            index = 0;
+        }
+        char = CHARS[index];
+        hash.update(char);
+        hash.update(args);
+        restore(hash);
+        ++index;
+    }
+    console.timeEnd('a')
+    postMessage([sig, id]);
+}
